76888e8d585f8e08af65218e3794f290a5d09ce3
mArcostxf <xaviermarcos33@gmail.com>
2023-10-28 10:26:47 -0300

    adição do cabeçalho

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
+/*Trabalho prático
+Introdução a algoritmos
+2023-2
+
+Feito por:
+Cleber
+Marcos Tadeu Xavier Ferreira - 201910448
+Max Deivid do Nascimento - 201910445
+*/
+
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
+
+
 int main () {
-    //eu estive aqui
+    
 
     return 0;
 }
