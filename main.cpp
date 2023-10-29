11181944e1aed9db76e7d8e98b4a52693fa71a5c
Maxnasc7 <maxnasc7@outlook.com>
2023-10-29 10:41:32 -0300

    Menu e chamada de funções adicionadas

--- main.cpp
+++ main.cpp
@@ -31,74 +31,92 @@ struct Galaxia
     string constelacao = "";
 };
 
-int verificarTamanhoArquivoCSV(const string& nomeArquivo) {
+int verificarTamanhoArquivoCSV(const string &nomeArquivo)
+{
     ifstream arquivo(nomeArquivo);
     int numeroDeLinhas = 0;
 
-    if (arquivo.is_open()) {
+    if (arquivo.is_open())
+    {
         string linha;
 
-        while (getline(arquivo, linha)) {
+        while (getline(arquivo, linha))
+        {
             numeroDeLinhas++;
         }
 
         arquivo.close();
 
         return numeroDeLinhas;
-    } else {
+    }
+    else
+    {
         cerr << "Erro ao abrir o arquivo." << endl;
         return -1; // Retorna -1 em caso de erro
     }
 }
 
-vector<Galaxia> lerCSV(const string& nomeArquivo) {
+vector<Galaxia> lerCSV(const string &nomeArquivo)
+{
     vector<Galaxia> galaxias;
     ifstream arquivo(nomeArquivo);
 
-    if (arquivo.is_open()) {
+    if (arquivo.is_open())
+    {
         string linha;
 
-        while (getline(arquivo, linha)) {
+        while (getline(arquivo, linha))
+        {
             Galaxia galaxia;
             istringstream linhaStream(linha);
             string campo;
 
             // Lê os campos da linha e atribui aos membros da struct
-            if (getline(linhaStream, campo, ',')) {
+            if (getline(linhaStream, campo, ','))
+            {
                 galaxia.nome_galaxia = campo;
             }
-            if (getline(linhaStream, campo, ',')) {
+            if (getline(linhaStream, campo, ','))
+            {
                 galaxia.tipo_galaxia = campo;
             }
-            if (getline(linhaStream, campo, ',')) {
+            if (getline(linhaStream, campo, ','))
+            {
                 galaxia.magnitude = stof(campo);
             }
-            if (getline(linhaStream, campo, ',')) {
+            if (getline(linhaStream, campo, ','))
+            {
                 galaxia.constelacao = campo;
             }
-            if (getline(linhaStream, campo, ',')) {
+            if (getline(linhaStream, campo, ','))
+            {
                 galaxia.identificador = stoi(campo);
             }
 
             galaxias.push_back(galaxia);
         }
-
+        cout << "Arquivo lido com sucesso!" << endl;
         arquivo.close();
-    } else {
+    }
+    else
+    {
         cerr << "Erro ao abrir o arquivo." << endl;
     }
 
     return galaxias;
 }
 
-void salvarCSV(vector<Galaxia> galaxias, string nomeArquivo) {
+void salvarCSV(vector<Galaxia> galaxias, string nomeArquivo)
+{
     std::ofstream arquivo(nomeArquivo);
 
-    if (arquivo.is_open()) {
+    if (arquivo.is_open())
+    {
         // Escreve o cabeçalho do arquivo CSV (opcional)
         arquivo << "Identificador, Nome da Galáxia, Tipo da Galáxia, Magnitude, Constelação" << std::endl;
 
-        for (const Galaxia& galaxia : galaxias) {
+        for (const Galaxia &galaxia : galaxias)
+        {
             arquivo << galaxia.identificador << ","
                     << galaxia.nome_galaxia << ","
                     << galaxia.tipo_galaxia << ","
@@ -108,35 +126,103 @@ void salvarCSV(vector<Galaxia> galaxias, string nomeArquivo) {
 
         arquivo.close();
         std::cout << "Dados salvos com sucesso em " << nomeArquivo << std::endl;
-    } else {
+    }
+    else
+    {
         std::cerr << "Erro ao criar o arquivo." << std::endl;
     }
 }
 
-int main () {
-    string nomeArquivoCSV = "/home/maxnas7/Documentos/IALG/trabalho/Trabalho_IALG_Galaxias/galaxys.csv";
-    string nomeArquivoCSVteste = "/home/maxnas7/Documentos/IALG/trabalho/Trabalho_IALG_Galaxias/galaxys_teste.csv";
-    int tamanhoCSV;
-    // Ler o arquivo .csv
-    tamanhoCSV = verificarTamanhoArquivoCSV(nomeArquivoCSV);
-    vector<Galaxia> galaxias;
+void menu(vector<Galaxia> galaxias, string nomeArquivoCSVimport, string nomeArquivoCSVexport)
+{
 
-    galaxias = lerCSV(nomeArquivoCSV);
+    // Bloco de indicação de quais as funções disponíveis para o usuário
+    cout << "Bem vindo ao sistema de gerenciamento de informações galácticas!" << endl;
+    int escolha = 100;
 
-    cout << tamanhoCSV << endl;
-    // ler_csv (); //Importar dados
-    // escreve_csv (); //Exportar dados
-
-    for (const Galaxia& galaxia : galaxias) {
-        cout << "Identificador: " << galaxia.identificador << endl;
-        cout << "Nome da Galáxia: " << galaxia.nome_galaxia << endl;
-        cout << "Tipo da Galáxia: " << galaxia.tipo_galaxia << endl;
-        cout << "Magnitude: " << galaxia.magnitude << endl;
-        cout << "Constelação: " << galaxia.constelacao << endl;
+    while (escolha != 0)
+    {
+        cout << endl << "====================================================================" << endl;
+        cout << "Escolha uma das opções:" << endl;
+        cout << "1  -> Importar dados de arquivo .csv" << endl;
+        cout << "2  -> Exportar dados para arquivo .csv" << endl;
+        cout << "3  -> Ordenar dados de acordo com característica especificada" << endl;
+        cout << "4  -> Inserir registro" << endl;
+        cout << "5  -> Apagar registro" << endl;
+        cout << "6  -> Buscar registro" << endl;
+        cout << "7  -> Exibir lista completa de registros" << endl;
+        cout << "8  -> Exibir lista parcial de registros" << endl;
+        cout << "9  -> Exibir modificação atual" << endl;
+        cout << "10 -> Salvar alterações" << endl;
+        cout << "0  -> Sair do programa" << endl;
+        cout << endl << "====================================================================" << endl;
+        cout << endl << "Opção: ";
+        cin >> escolha;
         cout << endl;
+        switch (escolha)
+        {
+        case 1: // Importar dados de arquivo .csv
+            galaxias = lerCSV(nomeArquivoCSVimport);
+            break;
+        case 2: // Exportar dados para arquivo .csv
+            salvarCSV(galaxias, nomeArquivoCSVexport);
+            break;
+        case 3: // Ordenar dados de acordo com característica especificada
+            cout << "Não implementado" << endl;
+            break;
+        case 4: // Inserir registro
+            cout << "Não implementado" << endl;
+            break;
+        case 5: // Apagar registro
+            cout << "Não implementado" << endl;
+            break;
+        case 6: // Buscar registro
+            cout << "Não implementado" << endl;
+            break;
+        case 7: // Exibir lista completa de registros
+            cout << "Não implementado" << endl;
+            break;
+        case 8: // Exibir lista parcial de registros
+            cout << "Não implementado" << endl;
+            break;
+        case 9: // Exibir lista parcial de registros
+            cout << "Não implementado" << endl;
+            break;
+        case 10: // Salvar alterações
+            cout << "Não implementado" << endl;
+            break;
+        case 0: // Exibir lista parcial de registros
+            cout << endl << "Obrigado! :)" << endl;
+            break;
+        default:
+            cout << "Opção não disponível" << endl;
+            break;
+        }
     }
+}
 
-    salvarCSV(galaxias, nomeArquivoCSVteste);
+int main()
+{
+    // Declaração de nomes de arquivos de import e export de .csv
+    string nomeArquivoCSVimport = "/home/maxnas7/Documentos/IALG/trabalho/Trabalho_IALG_Galaxias/galaxys_import.csv";
+    string nomeArquivoCSVexport = "/home/maxnas7/Documentos/IALG/trabalho/Trabalho_IALG_Galaxias/galaxys_export.csv";
+    int tamanhoCSV; // Verificador de galáxias cadastradas em arquivos .csv
+
+    // Verificar tamanho do arquivo .csv
+    // tamanhoCSV = verificarTamanhoArquivoCSV(nomeArquivoCSVimport);
+    // cout << tamanhoCSV << endl;
+
+    // Struct de dados
+    vector<Galaxia> galaxias;
+    menu(galaxias, nomeArquivoCSVimport, nomeArquivoCSVexport);
+    // for (const Galaxia& galaxia : galaxias) {
+    //     cout << "Identificador: " << galaxia.identificador << endl;
+    //     cout << "Nome da Galáxia: " << galaxia.nome_galaxia << endl;
+    //     cout << "Tipo da Galáxia: " << galaxia.tipo_galaxia << endl;
+    //     cout << "Magnitude: " << galaxia.magnitude << endl;
+    //     cout << "Constelação: " << galaxia.constelacao << endl;
+    //     cout << endl;
+    // }
 
     return 0;
 }
